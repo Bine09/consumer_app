bb01a543877c8a0529e818efa1b24535ba7d1161
Bine09 <s.immisch@gmx.de>
2019-02-05 20:07:47 +0100

    scaffolding and product in nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   get 'simple_pages/about'     # get the about site via the simple_pages controller
   get 'simple_pages/contact'
   get 'simple_pages/index'
