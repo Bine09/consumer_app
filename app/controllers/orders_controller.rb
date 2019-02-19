7ffd9493edf2b717bc701c2575551a5bd809a4f3
Bine09 <s.immisch@gmx.de>
2019-02-19 10:07:46 +0100

    4.6 added Orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,12 @@
 class OrdersController < ApplicationController    # The first line declares a class that inherits from ApplicationControlle,
   #meaning it has access to all the methods inside your application_controller.rb file
-  def index   # list of all orders?
+  def index   # list of multiple orders
+    @orders = Order.all
     end
 
-    def show  #show single order?
+    def show  #show order
+    @order = Order.find(params[:id])
+
     end
 
     def new
