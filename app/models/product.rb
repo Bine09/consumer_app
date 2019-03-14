32b74ff7b6c9691442354e016e2bb3c1c67c1e9f
Bine09 <s.immisch@gmx.de>
2019-03-14 12:01:38 +0100

    5.7 add comment model, scope and styling productdetail

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord  #means that your product class is a subclass of, or inherits from, the ApplicationRecord class.
+  has_many :comments
+
   def self.search(search_term)
     if  Rails.env.production?      #postgres db can make trouble by using "Like"
     Product.where("name ilike ?", "%#{search_term}%")
@@ -7,4 +9,12 @@ class Product < ApplicationRecord  #means that your product class is a subclass 
     end
   end
 
-end
+  def highest_rating_comment
+  comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+  comments.rating_asc.first
+  end
+
+  end
