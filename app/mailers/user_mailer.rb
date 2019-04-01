5e7867451eab83b70253470adf75a60c564fca1d
Bine09 <s.immisch@gmx.de>
2019-04-01 13:16:35 +0200

    added automated email on usersignup

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,12 @@ class UserMailer < ApplicationMailer
          to: 'bine09@gmx.de',
          subject: "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+  @appname = "Bike Shop"
+  mail(to: user.email,
+       subject: "Welcome to #{@appname}!")
+   end
+
+
 end
