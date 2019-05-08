1a5e8a5be34ee48c3f2d21f6c395facadfe879dc
Bine09 <s.immisch@gmx.de>
2019-05-08 12:10:19 +0200

    6.11 Caching

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+  def cache_key_for_products
+      count          = @products
+      max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
+      "products/all-#{count}-#{max_updated_at}"
+    end
+# When different parts of the page need to be cached and expired separately you can use Fragment Caching.
+
 end
