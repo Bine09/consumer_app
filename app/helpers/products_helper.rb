9249a9677cb6cafa98592c5992aabfa4fb0009a9
Bine09 <s.immisch@gmx.de>
2019-05-11 12:28:58 +0200

    add .length to cache_for_products helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,8 +1,8 @@
 module ProductsHelper
   def cache_key_for_products
-      count          = @products
-      max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
-      "products/all-#{count}-#{max_updated_at}"
+      count          = @products.length   #counts products in index
+      max_updated_at = @products.maximum(:updated_at).try(:utc).try(:to_s, :number) #looks for last updated product
+      "products/all-#{count}-#{max_updated_at}"  #generates cache key
     end
 # When different parts of the page need to be cached and expired separately you can use Fragment Caching.
 
